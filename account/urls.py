5b4ed1e833e433bff9000492e0fb2357699bc664
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-07-06 01:43:42 +0200

    add dashboard.html and LOGIN_REDIRECT_URL, LOGIN_URL, LOGOUT_URL

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login_from_book/', views.user_login, name='login_from_book'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
+    path('', views.dashboard, name='dashboard'),
+    ]
