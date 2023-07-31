e02c90cf1e673f3f702612318144358b96ff933c
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-07-31 22:14:20 +0200

    add change password template and path

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my application:
     'account',
+    # django app:
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
