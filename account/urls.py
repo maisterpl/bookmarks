937486126090995f146d2d43ecae0b406db5e7e0
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-08-08 11:44:56 +0200

    fix the password reset function

--- account/urls.py
+++ account/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+   
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
